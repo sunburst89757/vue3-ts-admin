b4481bd3f1297ef60503e625acf6b53852523184
sunburst <3520279278@qq.com>
2022-02-09 19:59:14 +0800

    初始化配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,18 +5,18 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/Home.vue"),
+    component: () => import("@/views/Home.vue")
   },
   {
     path: "/about",
     name: "About",
-    component: () => import("../views/About.vue"),
-  },
+    component: () => import("../views/About.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 });
 
 export default router;
