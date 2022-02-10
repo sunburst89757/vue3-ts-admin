591317638c5b98debdb0f361e9c851a9ac9585ea
sunburst <3520279278@qq.com>
2022-02-10 21:24:30 +0800

    feat(.env.devlopment/.env.production): 生产环境不同定义的变量

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,5 @@ register(app);
 app.use(store);
 app.use(router);
 app.mount("#app");
+console.log(process.env.VUE_APP_MY_NAME);
+console.log(process.env.VUE_APP_BASE_URL);
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,6 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+// 缺少类型的变量就直接在这里声明
+// declare const VUE_APP_MY_NAME:string;
+// declare const BASE_URL :string;
